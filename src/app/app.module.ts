79b942e39ec554a22aa92d08cdf56c0edd48acf5
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-04-12 12:55:26 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
@@ -23,16 +22,17 @@ import { MessageService } from './message.service';
     DashboardComponent
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule
 // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
  //and returns simulated server responses.
  //Remove it when a real server is ready to receive requests.
-HttpClientInMemoryWebApiModule.forRoot(
-  InMemoryDataService, { dataEncapsulation: false }
-)
+// HttpClientInMemoryWebApiModule.forRoot(
+//   InMemoryDataService, { dataEncapsulation: false }
+//)
 ],
   providers: [
     HeroService,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,36 @@
 import {Injectable} from '@angular/core';
 import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+//import {HEROES} from './mock-heroes';
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 import {MessageService} from './message.service';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' , 'user-key': '96ced68a4f764a6f8a19e953dae9bd55' },
+                          
+                                      
+)
+
+};
 @Injectable()
 export class HeroService {
 
 
   // Url to Web api
-  private heroesUrl = 'api/heroes';
+  private Url = 'https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318';
 
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.Url,httpOptions)
+    
 
   }
   getHero(id: number): Observable<Hero> {
-
-    return of(HEROES.find(hero => hero.id === id));
+  const url = '${this.Url}/${categories.categories.id}';
+  return this.http.get<Hero>(url);  
+  
   }
+  
 
   constructor(private http: HttpClient) {}
 
